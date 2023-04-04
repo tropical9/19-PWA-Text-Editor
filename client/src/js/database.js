eeb17e95b888de57f5139c5b201b46e114a6278e
tropical9 <113566952+tropical9@users.noreply.github.com>
2023-04-03 23:50:47 -0700

    deployable

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,7 +29,7 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 // export const getDb = async () => console.error('getDb not implemented');
 
-export const getAllDb = async () => {
+export const getDb = async () => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
